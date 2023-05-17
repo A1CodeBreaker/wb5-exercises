82f583ca07721e14f23c2bccd65c020ebc58e7a5
A1CodeBreaker <129992490+A1CodeBreaker@users.noreply.github.com>
2023-05-16 20:00:34 -0400

    multi array exercises

---
+++ MultiArrayScripts/multi_arrays.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let lockerAndAccessCode = [
+    ["Locker 1", 135],
+    ["Locker 2", 159],
+    ["Locker 3", 642],
+
+];
+
+console.log(lockerAndAcessCode[2] [1]);
+console.log(lockerAndAcessCode[1]);
